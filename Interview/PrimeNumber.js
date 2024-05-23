68a56c443bdce938ade302fcca89489b2d54e7b3
kavana <kavana94488@gmail.com>
2024-05-23 13:18:16 +0530

    correction

--- Interview/PrimeNumber.js
+++ Interview/PrimeNumber.js
@@ -2,7 +2,7 @@ function isPrime(num) {
     if (num <= 1) {
         return false
     }
-    for (let i = num; i <= Math.sqrt(num); i++) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
             return false
         }
